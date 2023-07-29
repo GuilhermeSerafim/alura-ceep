90dd088ff94db72ec84dadabd421f328dfa6be89
GuilhermeSerafim <guilerstudies@gmail.com>
2023-07-29 15:49:49 -0300

    Criando o componente de deletar a tarefa

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -14,7 +14,10 @@ const criarTarefa = (evento) => {
 
     tarefa.innerHTML = conteudo;
     
+    //Para possicionar o botão de acordo com o pai que é a "li"
     tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
+
     lista.appendChild(tarefa);
     input.value = " ";
 } 
@@ -47,5 +50,28 @@ const concluirTarefa = (evento) => {
     tarefaCompleta.classList.toggle('done'); //vai executar essa classe css, a partir do momento
     // em que eu clicar no botão | O metodo toggle devolve um booleano
 };
+
+//componente começa com letra maiscula
+const BotaoDeleta =  () => {
+    //minusculo pq é variavel
+    const botaoDeleta = document.createElement('button');
+    
+    botaoDeleta.classList.add('check-button');
+    botaoDeleta.innerText = "Deletar";
+
+    botaoDeleta.addEventListener('click', deletarTarefa);
+    return botaoDeleta;
+}
+
+const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target;
+
+    //Criado para pegar elemento li que é o pai do botão
+    //Precisamos pegar a li toda
+    const tarefaCompleta = botaoDeleta.parentElement;
+
+    tarefaCompleta.remove();
+    return botaoDeleta;
+}
 
 })()
